efcf208099b3fa54d3592ffa546f1945678d8232
Andy Huang <113555041+AndyHuang1205@users.noreply.github.com>
2023-03-26 19:43:56 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+3/26/2023
+CFB
+"""
+
+cipher_table = {
+    '0b00': '0b01',
+    '0b01': '0b10',
+    '0b10': '0b11',
+    '0b11': '0b00'
+}
+
+message = ["0b00", "0b01", "0b10", "0b11"]
+
+
+def CFB(message, IV="0b10"):
+    output = []
+    for m in message:
+        key = cipher_table.get(IV)
+        xor = '0b{:02b}'.format(int(key, 2) ^ int(m, 2))
+        IV = xor
+        print(xor)
+
+
+print(CFB(message))
