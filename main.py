ad8d29e4dc578cfe54c8f55e773727cb729df7a2
Andy Huang <113555041+AndyHuang1205@users.noreply.github.com>
2023-03-28 20:56:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,61 @@
 CFB
 """
 
-cipher_table = {
-    '0b00': '0b01',
-    '0b01': '0b10',
-    '0b10': '0b11',
-    '0b11': '0b00'
-}
 
-message = ["0b00", "0b01", "0b10", "0b11"]
+class CFB:
+    def __init__(self, message):
+        self.cipher_table = {
+            '0b00': '0b01',
+            '0b01': '0b10',
+            '0b10': '0b11',
+            '0b11': '0b00'
+        }
+        self.message = message
+        self.cipherText = []
+        self.IV = "0b10"
+
+    def toBinary(self, message):
+        binaryList = []
+        for c in message:
+            string = "{:08b}".format(ord(c))
+            for i in range(0, len(string), 2):
+                binaryList.append("0b" + string[i:i + 2])
+        return binaryList
+
+    def toString(self, message):
+        string = ""
+        start = 0
+        end = 8
+        while end < len(message)+1:
+            string += chr(int(message[start:end], 2))
+            start += 8
+            end += 8
+        return string
+
+    def CFB_cipher(self):
+        binaryMessage = self.toBinary(self.message)
+        initializationVector = self.IV
+        for m in binaryMessage:
+            key = self.cipher_table.get(initializationVector)
+            xor = '0b{:02b}'.format(int(key, 2) ^ int(m, 2))
+            initializationVector = xor
+            self.cipherText.append(xor)
+            print("The cipher text is: " + xor)
+
+    def CFB_decipher(self):
+        initializationVector = self.IV
+        plaintextBinary = ""
+        for b in self.cipherText:
+            key = self.cipher_table.get(initializationVector)
+            xor = '{:02b}'.format(int(key, 2) ^ int(b, 2))
+            initializationVector = b
+            plaintextBinary += xor
+            print("The plaintext in binary is: " + xor)
+        plaintext = self.toString(plaintextBinary)
+        return plaintext
 
 
-def CFB(message, IV="0b10"):
-    output = []
-    for m in message:
-        key = cipher_table.get(IV)
-        xor = '0b{:02b}'.format(int(key, 2) ^ int(m, 2))
-        IV = xor
-        print(xor)
-
-
-CFB(message)
+userInput = input("Enter plaintext: ")
+cfb = CFB(userInput)
+cfb.CFB_cipher()
+print("The decipher text is: " + cfb.CFB_decipher())
